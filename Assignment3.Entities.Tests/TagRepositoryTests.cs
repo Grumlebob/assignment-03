d842aba8e3daa6db918ffd728c9859f16f16fea7
Jacob Grum <jacg@itu.dk>
2022-09-28 15:01:19 +0200

    Alt virker

--- Assignment3.Entities.Tests/TagRepositoryTests.cs
+++ Assignment3.Entities.Tests/TagRepositoryTests.cs
@@ -1,19 +1,33 @@
+using Microsoft.Data.Sqlite;
+
 namespace Assignment3.Entities.Tests;
 
-public class TagRepositoryTests
+
+public sealed class TagRepositoryTests : IDisposable
 {
-    public readonly DbContextOptions<KanbanContext> dbContextOptions;
+    private readonly KanbanContext _context;
+    private readonly TagRepository _repository;
 
     public TagRepositoryTests()
     {
-        // Build DbContextOptions
-        dbContextOptions = new DbContextOptionsBuilder<KanbanContext>()
-            .UseInMemoryDatabase(databaseName: "InMemDB")
-            .Options;
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<KanbanContext>();
+        builder.UseSqlite(connection);
+        var context = new KanbanContext(builder.Options);
+        context.Database.EnsureCreated();
+        
+        context.Users.AddRange(new User() { Id = 1 , Name = "John", Email = "John@example.com" }, new User() { Id = 2 , Name = "Bob", Email = "Bob@example.com" });
+        //context.Characters.Add(new Character { Id = 1, AlterEgo = "Superman", CityId = 1 });
+        context.SaveChanges();
+
+        _context = context;
+        //_repository = new CityRepository(_context);
     }
 
-    [Fact]
-    public void update_or_delete_returns_null(){
-        
+
+    public void Dispose()
+    {
+        _context.Dispose();
     }
 }
--- Assignment3.Entities.Tests/UserRepositoryTests.cs
+++ Assignment3.Entities.Tests/UserRepositoryTests.cs
@@ -1,14 +1,43 @@
+using Assignment3.Core;
+
 namespace Assignment3.Entities.Tests;
 
-public class UserRepositoryTests
+public class UserRepositoryTests : IDisposable
 {
-    public readonly DbContextOptions<KanbanContext> dbContextOptions;
+    private readonly KanbanContext _context;
+    private readonly UserRepository _userRepository;
 
+    //Prøvet en philip route.
     public UserRepositoryTests()
     {
         // Build DbContextOptions
-        dbContextOptions = new DbContextOptionsBuilder<KanbanContext>()
+        var context = new DbContextOptionsBuilder<KanbanContext>()
             .UseInMemoryDatabase(databaseName: "InMemDB")
             .Options;
+        
+        _context = new KanbanContext(context);
+        _userRepository = new UserRepository(_context);
+
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        UserCreateDTO userCreateDTO = new UserCreateDTO(Email: "TestMail", Name: "TestName");
+        
+        // Act
+        var (response, userId) = _userRepository.Create(userCreateDTO);
+        
+        // Assert
+        Assert.Equal(Response.Created, response);
+        Assert.Equal(1, userId);
+        Assert.Equal(1, _context.Users.Count());
+        
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
     }
 }
