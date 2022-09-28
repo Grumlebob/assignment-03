53e850fededc2da1aed52fcbfd9229ba1cc483af
lufr@itu.dk <lufr@itu.dk>
2022-09-28 13:46:27 +0200

    fix inmem db

--- Assignment3.Entities.Tests/TaskRepositoryTests.cs
+++ Assignment3.Entities.Tests/TaskRepositoryTests.cs
@@ -2,5 +2,13 @@ namespace Assignment3.Entities.Tests;
 
 public class TaskRepositoryTests
 {
+    public readonly DbContextOptions<KanbanContext> dbContextOptions;
 
+    public TaskRepositoryTests()
+    {
+        // Build DbContextOptions
+        dbContextOptions = new DbContextOptionsBuilder<KanbanContext>()
+            .UseInMemoryDatabase(databaseName: "InMemDB")
+            .Options;
+    }
 }
--- Assignment3.Entities.Tests/UserRepositoryTests.cs
+++ Assignment3.Entities.Tests/UserRepositoryTests.cs
@@ -2,4 +2,13 @@ namespace Assignment3.Entities.Tests;
 
 public class UserRepositoryTests
 {
+    public readonly DbContextOptions<KanbanContext> dbContextOptions;
+
+    public UserRepositoryTests()
+    {
+        // Build DbContextOptions
+        dbContextOptions = new DbContextOptionsBuilder<KanbanContext>()
+            .UseInMemoryDatabase(databaseName: "InMemDB")
+            .Options;
+    }
 }
