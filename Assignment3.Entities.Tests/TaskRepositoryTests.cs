671045552c0424d9b222a29bc1b3138a4a801541
Jacob Grum <jacg@itu.dk>
2022-09-28 15:37:53 +0200

    Test repos updated

--- Assignment3.Entities.Tests/TaskRepositoryTests.cs
+++ Assignment3.Entities.Tests/TaskRepositoryTests.cs
@@ -1,14 +1,33 @@
+using Assignment3.Core;
+using Microsoft.Data.Sqlite;
+
 namespace Assignment3.Entities.Tests;
 
-public class TaskRepositoryTests
+
+public sealed class TaskRepositoryTests : IDisposable
 {
-    public readonly DbContextOptions<KanbanContext> dbContextOptions;
+    private readonly KanbanContext _context;
+    private readonly TaskRepository _repository;
 
     public TaskRepositoryTests()
     {
-        // Build DbContextOptions
-        dbContextOptions = new DbContextOptionsBuilder<KanbanContext>()
-            .UseInMemoryDatabase(databaseName: "InMemDB")
-            .Options;
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<KanbanContext>();
+        builder.UseSqlite(connection);
+        var context = new KanbanContext(builder.Options);
+        context.Database.EnsureCreated();
+        
+        context.Users.AddRange(new User() { Id = 1 , Name = "John", Email = "John@example.com" }, new User() { Id = 2 , Name = "Bob", Email = "Bob@example.com" });
+        context.SaveChanges();
+
+        _context = context;
+        _repository = new TaskRepository(_context);
+    }
+    
+    public void Dispose()
+    {
+        _context.Dispose();
     }
 }
+
--- Assignment3.Entities.Tests/UserRepositoryTests.cs
+++ Assignment3.Entities.Tests/UserRepositoryTests.cs
@@ -1,8 +1,31 @@
 using Assignment3.Core;
+using Microsoft.Data.Sqlite;
 
 namespace Assignment3.Entities.Tests;
 
-public class UserRepositoryTests
+public sealed class UserRepositoryTests : IDisposable
 {
+    private readonly KanbanContext _context;
+    private readonly UserRepository _repository;
 
+    public UserRepositoryTests()
+    {
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<KanbanContext>();
+        builder.UseSqlite(connection);
+        var context = new KanbanContext(builder.Options);
+        context.Database.EnsureCreated();
+        
+        context.Users.AddRange(new User() { Id = 1 , Name = "John", Email = "John@example.com" }, new User() { Id = 2 , Name = "Bob", Email = "Bob@example.com" });
+        context.SaveChanges();
+
+        _context = context;
+        _repository = new UserRepository(_context);
+    }
+    
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
 }
