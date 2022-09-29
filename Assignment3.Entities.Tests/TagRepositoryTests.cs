1cc931fb4f6ed7d90721a30f28aa6fe0eed744f7
lufr@itu.dk <lufr@itu.dk>
2022-09-29 17:15:59 +0200

    fixes

--- Assignment3.Entities.Tests/TagRepositoryTests.cs
+++ Assignment3.Entities.Tests/TagRepositoryTests.cs
@@ -17,29 +17,15 @@ public sealed class TagRepositoryTests : IDisposable
         builder.UseSqlite(connection);
         var context = new KanbanContext(builder.Options);
         context.Database.EnsureCreated();
-        
-        context.Users.AddRange(new User() { Id = 1 , Name = "John", Email = "John@example.com" }, new User() { Id = 2 , Name = "Bob", Email = "Bob@example.com" });
-        //context.Characters.Add(new Character { Id = 1, AlterEgo = "Superman", CityId = 1 });
         context.SaveChanges();
 
         _context = context;
         _repository = new TagRepository(_context);
     }
-    
-    [Fact]
-    public void CreateTag()
-    {
-        var (response, city) = _repository.Create(new TagCreateDTO(Name: "Bob"));
-        
-        Assert.Equal(1, _context.Tags.Count());
-        Assert.Equal("Bob", _context.Tags.First().Name);
-        Assert.Equal(1, _context.Tags.First().Id);
-        //comment
-    }
+ 
     [Fact]
     public void update_returns_update_or_notFound(){
-        _repository.Create(new TagCreateDTO(Name: "bob"));
-        
+        _repository.Create(new TagCreateDTO(Name: "bob"));   
 
         var tudto = new TagUpdateDTO(1, "tested");
         var actual =_repository.Update(tudto); 
