e80c667f24fc5952aa640da5ef0de1df958a7e7b
lufr@itu.dk <lufr@itu.dk>
2022-09-30 11:20:15 +0200

    fix

--- Assignment3.Entities/TaskRepository.cs
+++ Assignment3.Entities/TaskRepository.cs
@@ -18,16 +18,17 @@ public class TaskRepository : ITaskRepository
         var response = Response.Created;
         var newtask = new Task();
         try
-        {            
+        {
             if (context.Users.Find(task.AssignedToId) == null) return (Response.BadRequest, 0);
-            else{
-            newtask.Description = task.Description;
-            newtask.UserID = task.AssignedToId;
-            newtask.Title = task.Title;
-            newtask.State = State.New;
-            newtask.StateUpdated = DateTime.Now;
-            context.Tasks.Add(newtask);
-            context.SaveChanges();
+            else
+            {
+                newtask.Description = task.Description;
+                newtask.UserID = task.AssignedToId;
+                newtask.Title = task.Title;
+                newtask.State = State.New;
+                newtask.StateUpdated = DateTime.Now;
+                context.Tasks.Add(newtask);
+                context.SaveChanges();
             }
         }
         catch
@@ -41,85 +42,139 @@ public class TaskRepository : ITaskRepository
 
     public IReadOnlyCollection<TaskDTO> ReadAll()
     {
-        throw new NotImplementedException();
+        var all = new List<TaskDTO>();
+
+        foreach (var task in context.Tasks)
+        {
+            var tags = new List<String>();
+            foreach (var tag in task.Tags) tags.Add(tag.ToString()!);
+
+            all.Add(new TaskDTO(Id: task.Id, Title: task.Title!, AssignedToName: (context.Users.Find(task.UserID)!.Name!), Tags: tags, State: task.State));
+        }
+        if (all == null) return null!;
+        else return all;
     }
 
     public IReadOnlyCollection<TaskDTO> ReadAllRemoved()
     {
-        throw new NotImplementedException();
+        var all = new List<TaskDTO>();
+        foreach (var task in context.Tasks)
+        {
+            var tags = new List<String>();
+            foreach (var tag in task.Tags) tags.Add(tag.ToString()!);
+
+            if (task.State == State.Removed) all.Add(new TaskDTO(Id: task.Id, Title: task.Title!, AssignedToName: (context.Users.Find(task.UserID)!.Name!), Tags: tags, State: task.State));
+        }
+        if (all == null) return null!;
+        else return all;
     }
 
     public IReadOnlyCollection<TaskDTO> ReadAllByTag(string tag)
     {
-        throw new NotImplementedException();
+        var all = new List<TaskDTO>();
+        foreach (var task in context.Tasks)
+        {
+            var tags = new List<String>();
+            foreach (var t in task.Tags)
+            {
+                tags.Add(tag.ToString());
+                if (t.ToString() == tag) all.Add(new TaskDTO(Id: task.Id, Title: task.Title!, AssignedToName: (context.Users.Find(task.UserID)!.Name!), Tags: tags, State: task.State));
+            }
+        }
+        if (all == null) return null!;
+        else return all;
     }
 
     public IReadOnlyCollection<TaskDTO> ReadAllByUser(int userId)
     {
-        throw new NotImplementedException();
+        var all = new List<TaskDTO>();
+        foreach (var task in context.Tasks)
+        {
+            var tags = new List<String>();
+            foreach (var tag in task.Tags) tags.Add(tag.ToString()!);
+            if (task.UserID == userId) all.Add(new TaskDTO(Id: task.Id, Title: task.Title!, AssignedToName: (context.Users.Find(task.UserID)!.Name!), Tags: tags, State: task.State));
+
+        }
+        if (all == null) return null!;
+        else return all;
     }
 
     public IReadOnlyCollection<TaskDTO> ReadAllByState(State state)
     {
-        throw new NotImplementedException();
+        var all = new List<TaskDTO>();
+        foreach (var task in context.Tasks)
+        {
+            var tags = new List<String>();
+            foreach (var tag in task.Tags) tags.Add(tag.ToString()!);
+            if (task.State == state) all.Add(new TaskDTO(Id: task.Id, Title: task.Title!, AssignedToName: (context.Users.Find(task.UserID)!.Name!), Tags: tags, State: task.State));
+        }
+        if (all == null) return null!;
+        else return all;
     }
 
     public TaskDetailsDTO Read(int taskId)
     {
-        throw new NotImplementedException();
+        foreach (var task in context.Tasks)
+        {
+            var tags = new List<String>();
+            foreach (var tag in task.Tags) tags.Add(tag.ToString()!);
+            if (task.Id == taskId)
+            {
+                return new TaskDetailsDTO(Id: task.Id, Title: task.Title!, Description: task.Description!, Created: task.StateUpdated, AssignedToName: (context.Users.Find(task.UserID)!.Name)!, Tags: tags, State: task.State, task.StateUpdated);
+            }
+        }return null!;
     }
-
-    public Response Update(TaskUpdateDTO task)
-    {
-        try
+        public Response Update(TaskUpdateDTO task)
         {
-            var newTask = context.Tasks.Where(t => t.Id == task.Id).First();
-            if (newTask.State != task.State)
+            try
             {
-                newTask.State = task.State;
-                newTask.StateUpdated = DateTime.Now;
-            }
-            if (context.Users.Find(task.AssignedToId) == null) return Response.BadRequest;
-            else
-            {
-                if (task.Tags != null) newTask.Tags = (List<Tag>)task.Tags;
+                var newTask = context.Tasks.Where(t => t.Id == task.Id).First();
+                if (newTask.State != task.State)
+                {
+                    newTask.State = task.State;
+                    newTask.StateUpdated = DateTime.Now;
+                }
+                if (context.Users.Find(task.AssignedToId) == null) return Response.BadRequest;
+                else
+                {
+                    if (task.Tags != null) newTask.Tags = (List<Tag>)task.Tags;
 
-                newTask.Description = task.Description;
-                newTask.Title = task.Title;
-                newTask.UserID = task.AssignedToId;
-                context.Tasks.Update(newTask);
-                context.SaveChanges();
-                return Response.Updated;
+                    newTask.Description = task.Description;
+                    newTask.Title = task.Title;
+                    newTask.UserID = task.AssignedToId;
+                    context.Tasks.Update(newTask);
+                    context.SaveChanges();
+                    return Response.Updated;
+                }
+            }
+            catch
+            {
+                return Response.NotFound;
             }
         }
-        catch
+
+        public Response Delete(int taskId)
         {
-            return Response.NotFound;
+            try
+            {
+                var newTask = context.Tasks.Where(t => t.Id == taskId).First();
+                if (newTask!.State == State.New)
+                {
+                    context.Remove(newTask!);
+                    context.SaveChanges();
+                    return Response.Deleted;
+                }
+                else if (newTask!.State == State.Active)
+                {
+                    newTask.State = State.Removed;
+                    return Response.Conflict;
+                }
+                else return Response.Conflict;
+
+            }
+            catch
+            {
+                return Response.NotFound;
+            }
         }
     }
-
-    public Response Delete(int taskId)
-    {
-        try
-        {
-            var newTask = context.Tasks.Where(t => t.Id == taskId).First();
-            if (newTask!.State == State.New)
-            {
-                context.Remove(newTask!);
-                context.SaveChanges();
-                return Response.Deleted;
-            }
-            else if (newTask!.State == State.Active)
-            {
-                newTask.State = State.Removed;
-                return Response.Conflict;
-            }
-            else return Response.Conflict;
-
-        }
-        catch
-        {
-            return Response.NotFound;
-        }
-    }
-}
